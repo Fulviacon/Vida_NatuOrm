4c7cc7f175850bcaa8e9bcafaf25c62307d92e49
Your Name <you@example.com>
2024-05-14 21:16:14 -0300

    Conexión a la BD ONline

--- VidaNatural_BackEnd-main/src/app.module.ts
+++ VidaNatural_BackEnd-main/src/app.module.ts
@@ -11,16 +11,25 @@ import { Persona } from './entities/persona.entity';
 import { DonacionesModule } from './donaciones/donaciones.module';
 import { Donaciones } from './entities/donaciones.entity';
 import { Mensaje } from './entities/mensajes.entity';
+import { URL } from "url";
+
+const dbUrl = new URL("postgresql://vida-natural2024:REVEAL_PASSWORD@vida-natural-7555.g8z.gcp-us-east1.cockroachlabs.cloud:26257/vida_natural?sslmode=verify-full");
+const routingId = dbUrl.searchParams.get("options");
+dbUrl.searchParams.delete("options");
 
 @Module ({
   imports: [
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "root",
+      type: "cockroachdb",
+      host: "vida-natural-7555.g8z.gcp-us-east1.cockroachlabs.cloud",
+      port: 26257 ,
+      username: "vida-natural2024",
+      password: "TZs2Ywa_bVv0C7tZYtXOvg",
       database: "vida_natural",
+      ssl: true,
+  extra: {
+    options: routingId
+  },
       entities: [Persona,Donaciones,Mensaje],//__dirname + "/entity/*{.js,.ts}"
       synchronize:true
         }),
