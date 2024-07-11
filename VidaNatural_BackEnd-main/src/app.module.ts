67db7ce6739e60f633e23fba784ee07db743f1fd
Your Name <you@example.com>
2024-07-11 20:01:41 -0300

    Cambios de puerto a Render

--- VidaNatural_BackEnd-main/src/app.module.ts
+++ VidaNatural_BackEnd-main/src/app.module.ts
@@ -1,20 +1,48 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { PersonaModule } from './persona/persona.module';
 import { AppController} from './app.controller';
 import { AppService } from './app.service';
-
 import { MensajeModule } from './mensaje/mensaje.module';
-
 import { Persona } from './entities/persona.entity';
 import { DonacionesModule } from './donaciones/donaciones.module';
 import { Donaciones } from './entities/donaciones.entity';
 import { Mensaje } from './entities/mensajes.entity';
-import { URL } from "url";
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-const dbUrl = new URL("postgresql://vida-natural2024:REVEAL_PASSWORD@vida-natural-7555.g8z.gcp-us-east1.cockroachlabs.cloud:26257/vida_natural?sslmode=verify-full");
+//import { URL } from "url";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'cockroachdb',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        ssl:true,
+        entities: [Persona,Donaciones,Mensaje],//__dirname + "/entity/*{.js,.ts}"
+      synchronize:false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
+
+
+
+/*const dbUrl = new URL("postgresql://vida_natural_2024:REVEAL_PASSWORD@vida-natural-7555.g8z.gcp-us-east1.cockroachlabs.cloud:26257/vida_natural?sslmode=verify-full");
 const routingId = dbUrl.searchParams.get("options");
 dbUrl.searchParams.delete("options");
 
@@ -24,7 +52,7 @@ dbUrl.searchParams.delete("options");
       type: "cockroachdb",
       host: "vida-natural-7555.g8z.gcp-us-east1.cockroachlabs.cloud",
       port: 26257 ,
-      username: "vida-natural2024",
+      username: "vida_natural_2024",
       password: "TZs2Ywa_bVv0C7tZYtXOvg",
       database: "vida_natural",
       ssl: true,
@@ -42,4 +70,4 @@ dbUrl.searchParams.delete("options");
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {}*/
