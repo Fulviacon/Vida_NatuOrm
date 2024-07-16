8312cae6df13a258a349c73159970d47e2f852cb
Your Name <you@example.com>
2024-07-16 18:58:51 -0300

    guardando cambios

--- VidaNatural_BackEnd-main/src/persona/persona.controller.ts
+++ VidaNatural_BackEnd-main/src/persona/persona.controller.ts
@@ -1,52 +1,48 @@
-import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode,  HttpStatus,  ParseIntPipe, HttpException,} from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode, HttpStatus, ParseIntPipe, HttpException } from '@nestjs/common';
 import { PersonaDTO } from './persona.dto';
 import { Persona } from 'src/entities/persona.entity';
 import { PersonaService } from './persona.service';
 
-
 @Controller('persona')
 export class PersonaController {
   constructor(private readonly personaService: PersonaService) {}
-  //trae todas las personas
+
+  // Trae todas las personas
   @Get()
   @HttpCode(200)
   async getPersonas(): Promise<Persona[]> {
     return await this.personaService.getPersona();
   }
 
-//trae una persona por id
+  // Trae una persona por id
   @Get(':id')
   async getPersonaById(@Param('id', new ParseIntPipe()) id: number): Promise<Persona> {
     const persona = await this.personaService.getByIdPersona(id);
-    if (persona) return persona;
-    
-  
-}
+    if (persona) {
+      return persona;
+    } else {
+      throw new HttpException('Persona no encontrada', HttpStatus.NOT_FOUND);
+    }
+  }
 
-
-//crea una persona
+  // Crea una persona
   @Post()
   @HttpCode(201)
-  async crearPersona(@Body() PersonaDto: PersonaDTO): Promise<Persona> {
-    return await this.personaService.crearPersona(PersonaDto);
+  async crearPersona(@Body() personaDto: PersonaDTO): Promise<Persona> {
+    return await this.personaService.crearPersona(personaDto);
   }
 
-  //borra una persona
-
+  // Borra una persona
   @Delete(':id')
   @HttpCode(200)
-  async deletePersonaByID( @Param( 'id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
-    )  id: number, datos:PersonaDTO ):Promise<string> {
-   return await this.personaService.deletePersona(id,datos);
-   
+  async deletePersonaByID(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number): Promise<string> {
+    await this.personaService.deletePersona(id);
+    return 'Persona eliminada correctamente';
   }
 
-  //modifcica datos existentes
+  // Modifica datos existentes
   @Put(':id')
-  async updatePersonById(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })
-) id: number, @Body() personaDto: PersonaDTO ): Promise<Persona> {
-      return await this.personaService.updatePersona(id, personaDto);
-    
+  async updatePersonById(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number, @Body() personaDto: PersonaDTO): Promise<Persona> {
+    return await this.personaService.updatePersona(id, personaDto);
   }
 }
-
--- VidaNatural_BackEnd-main/src/user/user.service.ts
+++ VidaNatural_BackEnd-main/src/user/user.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
   private readonly users = [
-    //{ id: 1, name: 'John Doe', email: 'john@example.com', password: 'password123' },
-   // { id: 2, name: 'Jane Doe', email: 'jane@example.com', password: 'password456' },
+    { id: 1, name: 'John Doe', email: 'john@example.com', password: 'password123' },
+    { id: 2, name: 'Jane Doe', email: 'jane@example.com', password: 'password456' },
   ];
 
   findAll() {
